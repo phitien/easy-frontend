9e528b63d71b646281b8d9326f9bf48b8416d3d3
Phi Tien <phi.tien@goldenequator.com>
2017-09-18 01:19:51 +0800

    Not to remove ezy command when use ezy uninstall

--- bin/uninstall.js
+++ bin/uninstall.js
@@ -1,10 +1,8 @@
 module.exports = exports = function(config) {
     const {exec} = require('child_process')
-    console.log(`EZY Removing ..., path: ${process.env.HOME}/${config.EZY_HOME}`)
-    exec(`cd ${process.env.HOME}/${config.EZY_HOME} && npm uninstall -g ezy`, (err, stdout, stderr) => {
-        exec(`rm -rf ${process.env.HOME}/${config.EZY_HOME} && rm -rf $(which ezy)`, (err, stdout, stderr) => {
-            console.log(`EZY Done Removing`)
-            process.exit(0)
-        })
+    console.log(`EZY Removing ...`)
+    exec(`rm -rf ${process.env.HOME}/${config.EZY_HOME}`, (err, stdout, stderr) => {
+        console.log(`EZY Done Removing`)
+        process.exit(0)
     })
 }
